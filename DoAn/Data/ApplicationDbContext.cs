ba6752eb8a621cdb161575b063c3c93df26cc812
NhuyNhuy63 <lethingocnhi632@gmail.com>
2023-12-14 17:05:36 +0700

    Add project files.

---
+++ DoAn/Data/ApplicationDbContext.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using DoAn.Models;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DoAn.Data
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Chucvu> Chucvu { get; set; }
+        public virtual DbSet<Cthoadon> Cthoadon { get; set; }
+        public virtual DbSet<Cuahang> Cuahang { get; set; }
+        public virtual DbSet<Danhmuc> Danhmuc { get; set; }
+        public virtual DbSet<Diachi> Diachi { get; set; }
+        public virtual DbSet<Hoadon> Hoadon { get; set; }
+        public virtual DbSet<Khachhang> Khachhang { get; set; }
+        public virtual DbSet<Mathang> Mathang { get; set; }
+        public virtual DbSet<Nhanvien> Nhanvien { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=LAPTOP-0QGME0LQ;Initial Catalog=banhkem;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Chucvu>(entity =>
+            {
+                entity.HasKey(e => e.MaCv)
+                    .HasName("PK__CHUCVU__27258E76D78834E8");
+
+                entity.Property(e => e.HeSo).HasDefaultValueSql("((1.0))");
+            });
+
+            modelBuilder.Entity<Cthoadon>(entity =>
+            {
+                entity.HasKey(e => e.MaCthd)
+                    .HasName("PK__CTHOADON__1E4FA77102E52569");
+
+                entity.Property(e => e.DonGia).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.SoLuong).HasDefaultValueSql("((1))");
+
+                entity.HasOne(d => d.MaHdNavigation)
+                    .WithMany(p => p.Cthoadon)
+                    .HasForeignKey(d => d.MaHd)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__CTHOADON__MaHD__5441852A");
+
+                entity.HasOne(d => d.MaMhNavigation)
+                    .WithMany(p => p.Cthoadon)
+                    .HasForeignKey(d => d.MaMh)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__CTHOADON__MaMH__5535A963");
+            });
+
+            modelBuilder.Entity<Cuahang>(entity =>
+            {
+                entity.HasKey(e => e.MaCh)
+                    .HasName("PK__CUAHANG__27258E00EB114DF2");
+
+                entity.Property(e => e.DienThoai).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Danhmuc>(entity =>
+            {
+                entity.HasKey(e => e.MaDm)
+                    .HasName("PK__DANHMUC__2725866EC6B98D48");
+            });
+
+            modelBuilder.Entity<Diachi>(entity =>
+            {
+                entity.HasKey(e => e.MaDc)
+                    .HasName("PK__DIACHI__27258664ADC8A973");
+
+                entity.Property(e => e.MacDinh).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.PhuongXa).IsUnicode(false);
+
+                entity.Property(e => e.QuanHuyen).IsUnicode(false);
+
+                entity.Property(e => e.TinhThanh).IsUnicode(false);
+
+                entity.HasOne(d => d.MaKhNavigation)
+                    .WithMany(p => p.Diachi)
+                    .HasForeignKey(d => d.MaKh)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__DIACHI__MaKH__4AB81AF0");
+            });
+
+            modelBuilder.Entity<Hoadon>(entity =>
+            {
+                entity.HasKey(e => e.MaHd)
+                    .HasName("PK__HOADON__2725A6E00B2DFA45");
+
+                entity.Property(e => e.Ngay).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.TongTien).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.TrangThai).HasDefaultValueSql("((0))");
+
+                entity.HasOne(d => d.MaKhNavigation)
+                    .WithMany(p => p.Hoadon)
+                    .HasForeignKey(d => d.MaKh)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__HOADON__MaKH__5070F446");
+            });
+
+            modelBuilder.Entity<Khachhang>(entity =>
+            {
+                entity.HasKey(e => e.MaKh)
+                    .HasName("PK__KHACHHAN__2725CF1EE1662B6E");
+
+                entity.Property(e => e.DienThoai).IsUnicode(false);
+
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.MatKhau).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Mathang>(entity =>
+            {
+                entity.HasKey(e => e.MaMh)
+                    .HasName("PK__MATHANG__2725DFD947A18F5E");
+
+                entity.Property(e => e.GiaBan).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.GiaGoc).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.HinhAnh).IsUnicode(false);
+
+                entity.Property(e => e.LuotMua).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.LuotXem).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.SoLuong).HasDefaultValueSql("((0))");
+
+                entity.HasOne(d => d.MaDmNavigation)
+                    .WithMany(p => p.Mathang)
+                    .HasForeignKey(d => d.MaDm)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__MATHANG__MaDM__3E52440B");
+            });
+
+            modelBuilder.Entity<Nhanvien>(entity =>
+            {
+                entity.HasKey(e => e.MaNv)
+                    .HasName("PK__NHANVIEN__2725D70ACE7E95FE");
+
+                entity.Property(e => e.DienThoai).IsUnicode(false);
+
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.MatKhau).IsUnicode(false);
+
+                entity.HasOne(d => d.MaCvNavigation)
+                    .WithMany(p => p.Nhanvien)
+                    .HasForeignKey(d => d.MaCv)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__NHANVIEN__MaCV__45F365D3");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}

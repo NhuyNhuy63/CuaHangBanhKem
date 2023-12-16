a97e3962c8b9a0472b94a7176877f705a369b47f
Minh-Thach <tmthach_21th@student.agu.edu.vn>
2023-12-16 17:14:08 +0700

    qlsanpham_231216

---
+++ DoAn/Controllers/QLMathangsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DoAn.Data;
+using DoAn.Models;
+
+namespace DoAn.Controllers
+{
+    public class QLMathangsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QLMathangsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: QLMathangs
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Mathang.Include(m => m.MaDmNavigation);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: QLMathangs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mathang = await _context.Mathang
+                .Include(m => m.MaDmNavigation)
+                .FirstOrDefaultAsync(m => m.MaMh == id);
+            if (mathang == null)
+            {
+                return NotFound();
+            }
+
+            return View(mathang);
+        }
+
+        // GET: QLMathangs/Create
+        public IActionResult Create()
+        {
+            ViewData["MaDm"] = new SelectList(_context.Danhmuc, "MaDm", "Ten");
+            return View();
+        }
+
+        // POST: QLMathangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaMh,Ten,GiaGoc,GiaBan,SoLuong,MoTa,HinhAnh,MaDm,LuotXem,LuotMua")] Mathang mathang)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(mathang);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaDm"] = new SelectList(_context.Danhmuc, "MaDm", "Ten", mathang.MaDm);
+            return View(mathang);
+        }
+
+        // GET: QLMathangs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mathang = await _context.Mathang.FindAsync(id);
+            if (mathang == null)
+            {
+                return NotFound();
+            }
+            ViewData["MaDm"] = new SelectList(_context.Danhmuc, "MaDm", "Ten", mathang.MaDm);
+            return View(mathang);
+        }
+
+        // POST: QLMathangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaMh,Ten,GiaGoc,GiaBan,SoLuong,MoTa,HinhAnh,MaDm,LuotXem,LuotMua")] Mathang mathang)
+        {
+            if (id != mathang.MaMh)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mathang);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MathangExists(mathang.MaMh))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaDm"] = new SelectList(_context.Danhmuc, "MaDm", "Ten", mathang.MaDm);
+            return View(mathang);
+        }
+
+        // GET: QLMathangs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mathang = await _context.Mathang
+                .Include(m => m.MaDmNavigation)
+                .FirstOrDefaultAsync(m => m.MaMh == id);
+            if (mathang == null)
+            {
+                return NotFound();
+            }
+
+            return View(mathang);
+        }
+
+        // POST: QLMathangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var mathang = await _context.Mathang.FindAsync(id);
+            _context.Mathang.Remove(mathang);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MathangExists(int id)
+        {
+            return _context.Mathang.Any(e => e.MaMh == id);
+        }
+    }
+}

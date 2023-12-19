7aaea1404c29d835806652fd94ab78d28db21167
nguyen17-th <dangnnguyen17@gmail.com>
2023-12-19 17:37:09 +0700

    QLnhanvien_231219

---
+++ DoAn/Controllers/QLNhanviensController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DoAn.Data;
+using DoAn.Models;
+
+namespace DoAn.Controllers
+{
+    public class QLNhanviensController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QLNhanviensController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: QLNhanviens
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Nhanvien.Include(n => n.MaCvNavigation);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: QLNhanviens/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nhanvien = await _context.Nhanvien
+                .Include(n => n.MaCvNavigation)
+                .FirstOrDefaultAsync(m => m.MaNv == id);
+            if (nhanvien == null)
+            {
+                return NotFound();
+            }
+
+            return View(nhanvien);
+        }
+
+        // GET: QLNhanviens/Create
+        public IActionResult Create()
+        {
+            ViewData["MaCv"] = new SelectList(_context.Chucvu, "MaCv", "Ten");
+            return View();
+        }
+
+        // POST: QLNhanviens/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaNv,Ten,MaCv,DienThoai,Email,MatKhau")] Nhanvien nhanvien)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(nhanvien);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaCv"] = new SelectList(_context.Chucvu, "MaCv", "Ten", nhanvien.MaCv);
+            return View(nhanvien);
+        }
+
+        // GET: QLNhanviens/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nhanvien = await _context.Nhanvien.FindAsync(id);
+            if (nhanvien == null)
+            {
+                return NotFound();
+            }
+            ViewData["MaCv"] = new SelectList(_context.Chucvu, "MaCv", "Ten", nhanvien.MaCv);
+            return View(nhanvien);
+        }
+
+        // POST: QLNhanviens/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaNv,Ten,MaCv,DienThoai,Email,MatKhau")] Nhanvien nhanvien)
+        {
+            if (id != nhanvien.MaNv)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(nhanvien);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NhanvienExists(nhanvien.MaNv))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaCv"] = new SelectList(_context.Chucvu, "MaCv", "Ten", nhanvien.MaCv);
+            return View(nhanvien);
+        }
+
+        // GET: QLNhanviens/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nhanvien = await _context.Nhanvien
+                .Include(n => n.MaCvNavigation)
+                .FirstOrDefaultAsync(m => m.MaNv == id);
+            if (nhanvien == null)
+            {
+                return NotFound();
+            }
+
+            return View(nhanvien);
+        }
+
+        // POST: QLNhanviens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var nhanvien = await _context.Nhanvien.FindAsync(id);
+            _context.Nhanvien.Remove(nhanvien);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NhanvienExists(int id)
+        {
+            return _context.Nhanvien.Any(e => e.MaNv == id);
+        }
+    }
+}
